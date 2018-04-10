044d254475aef42ff57d50d857b65dbf5b65874b
peter-z-wang <peter.wang@colorado.edu>
2018-04-10 02:16:02 -0600

    Add Image Display

--- display.php
+++ display.php
@@ -1,1 +1,27 @@
-display.php
+<!DOCTYPE html>
+<html lang="en">
+<style>
+	body{
+		zoom: 100%
+	}
+</style>
+
+<head>
+
+	<title> Display </title>
+
+
+</head>
+
+<body>
+
+<?php
+
+echo "PHP Script";
+
+?>
+
+
+</body>
+
+</html>
